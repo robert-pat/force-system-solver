ba54f89a084498f4db397b70373ceea75b5c7943
phoenixPilot <5555bobjoe@gmail.com>
2024-03-25 23:45:02 -0400

    don't write code till its needed / used

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,3 @@ mod tests;
 fn main() {
     println!("Hello, World!");
 }
-
-#[allow(unused)]
-fn ask_for_file() -> Option<String> {
-    println!("Statics Truss Solver | ENGR 2120H");
-    println!("Please enter a file name (relative to this directory):");
-
-    let mut s = String::new();
-    match std::io::stdin().read_line(&mut s) {
-        Ok(_) => {}
-        Err(e) => {
-            eprintln!("Error in asking user for file at startup!");
-            eprintln!("Tried to read from standard in: {e}");
-            return None;
-        }
-    };
-
-    let read_file = match std::fs::read_to_string(&s) {
-        Ok(file) => file,
-        Err(e) => {
-            eprintln!("Error reading file \'{s}\' to being solving!");
-            eprintln!("Saw error: {e}");
-            return None;
-        }
-    };
-
-    Some(read_file)
-}
