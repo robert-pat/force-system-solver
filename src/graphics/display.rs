0fd30f790914eb72989a490f77adc3c1b28b1545
phoenixPilot <5555bobjoe@gmail.com>
2024-06-25 16:44:21 -0400

    clear display stuff for later

--- src/graphics/display.rs
+++ src/graphics/display.rs
@@ -1,5 +1,1 @@
-use std::collections::HashMap;
-
-use crate::solver::{Direction2D, Force2D, Point2D, SolverID, TrussJoint2D};
-
 mod render_backing;
---
+++ src/graphics/render_backing.rs
@@ -0,0 +1,6 @@
+#![allow(dead_code)]
+
+struct App {}
+impl App {}
+
+struct AppData{}
