a345c74389f11eaf6b578c92f5f37cd86123a7d8
phoenixPilot <5555bobjoe@gmail.com>
2024-07-12 23:09:49 -0400

    moved drawing settings from consts to struct

--- src/display.rs
+++ src/display.rs
@@ -21,77 +21,126 @@ pub fn display_truss_and_exit(truss: Truss2D) -> ! {
     std::process::exit(0);
 }
 
+#[derive(Clone, Copy, Debug)]
+struct TrussDrawSettings {
+    screen_padding: f32,
+    point_width: f32,
+    point_height: f32,
+    member_thickness: f32,
+    load_mag_scalar: f32,
+    load_thickness: f32,
+    load_arrow_scale: f32,
+    pin_scale: f32,
+    roller_scale: f32,
+}
+impl TrussDrawSettings {
+    const fn new(options: [f32; 9]) -> Self {
+        TrussDrawSettings {
+            screen_padding: options[0],
+            point_width: options[1],
+            point_height: options[2],
+            member_thickness: options[3],
+            load_mag_scalar: options[4],
+            load_thickness: options[5],
+            load_arrow_scale: options[6],
+            pin_scale: options[7],
+            roller_scale: options[8],
+        }
+    }
+}
+impl Default for TrussDrawSettings {
+    fn default() -> Self {
+        TrussDrawSettings {
+            screen_padding: 60_f32,
+            point_width: 18_f32,
+            point_height: 18_f32,
+            member_thickness: 9.5_f32,
+            load_mag_scalar: 1_f32,
+            load_thickness: 5_f32,
+            load_arrow_scale: 10_f32,
+            pin_scale: 6.5_f32,
+            roller_scale: 10_f32,
+        }
+    }
+}
+
 /// Draws the given truss to the screen with macroquad.
-fn draw_truss(truss: &Truss2D) {
-    const SCREEN_PADDING: f32 = 60_f32;
+fn draw_truss(truss: &Truss2D, settings: TrussDrawSettings) {
     let screen_bounds = (mq::screen_width(), mq::screen_height());
     mq::clear_background(mq::BEIGE);
-    let coordinate_map = make_coordinate_map(truss, screen_bounds, SCREEN_PADDING);
+    let coordinate_map = make_coordinate_map(truss, screen_bounds, settings.screen_padding);
 
     for point in truss.points.values() {
-        const POINT_WIDTH: f32 = 18_f32;
-        const POINT_HEIGHT: f32 = 18_f32;
-
+        let (width, height) = (settings.point_width, settings.point_height);
         let (x, y) = coordinate_map(point.pos());
-        mq::draw_rectangle(
-            x - 0.5 * POINT_WIDTH,
-            y - 0.5 * POINT_HEIGHT,
-            POINT_WIDTH,
-            POINT_HEIGHT,
-            mq::BLACK,
-        );
+        mq::draw_rectangle(x - 0.5 * width, y - 0.5 * height, width, height, mq::BLACK);
     }
 
-    const MEMBER_THICKNESS: f32 = 9.5_f32;
     for (p1, p2) in truss.connections.iter() {
         let (x1, y1) = coordinate_map(truss.points.get(p1).unwrap().pos());
         let (x2, y2) = coordinate_map(truss.points.get(p2).unwrap().pos());
 
-        mq::draw_line(x1, y1, x2, y2, MEMBER_THICKNESS, mq::BLACK);
+        mq::draw_line(x1, y1, x2, y2, settings.member_thickness, mq::BLACK);
     }
 
-    const LOAD_MAG_SCALAR: f32 = 1_f32; // eventually will need to be non-const
-    const LOAD_THICKNESS: f32 = 5_f32;
-    const LOAD_ARROW_SCALE: f32 = 10_f32;
     for load in truss.loads.values() {
         let (x, y) = coordinate_map(truss.points.get(&load.at).unwrap().pos());
         // Invert the y-dir for screen space conversion (coordinate_map already does for pos)
         let (x_dir, y_dir) = (load.dir.x as f32, -load.dir.y as f32);
         let mag = load.mag.magnitude().unwrap() as f32;
         let (end_x, end_y) = (
-            x + mag * x_dir * LOAD_MAG_SCALAR,
-            y + mag * y_dir * LOAD_MAG_SCALAR,
+            x + mag * x_dir * settings.load_mag_scalar,
+            y + mag * y_dir * settings.load_mag_scalar,
         );
-        mq::draw_line(x, y, end_x, end_y, LOAD_THICKNESS, mq::BROWN);
+        mq::draw_line(x, y, end_x, end_y, settings.load_thickness, mq::BROWN);
 
         // vector from origin to tip of arrow / end of line
         let start = mq::vec2(end_x, end_y);
-        let tri = triangle_with_rotation(load.dir, LOAD_ARROW_SCALE);
-        
+        let tri = triangle_with_rotation(load.dir, settings.load_arrow_scale);
+
         mq::draw_triangle(start + tri[0], start + tri[1], start + tri[2], mq::BROWN);
     }
 
-    const PIN_SCALE: f32 = 6.5_f32;
-    const ROLLER_SCALE: f32 = 10_f32;
     for support in truss.supports.values() {
         let (x, y) = coordinate_map(truss.points.get(&support.at()).unwrap().pos());
         match support {
             Support::Pin { .. } => {
-                mq::draw_circle(x, y, PIN_SCALE, mq::BLUE);
+                mq::draw_circle(x, y, settings.pin_scale, mq::BLUE);
             }
             Support::Roller { dir, .. } => {
                 let start = mq::vec2(x, y);
-                let tri = triangle_with_rotation(*dir, ROLLER_SCALE);
+                let tri = triangle_with_rotation(*dir, settings.roller_scale);
                 mq::draw_triangle(start + tri[0], start + tri[1], start + tri[2], mq::BLUE);
             }
         }
     }
 }
 
+
+
+fn draw_labels(truss: &Truss2D, settings: TrussDrawSettings) {
+    let screen_bounds = (mq::screen_width(), mq::screen_height());
+    let to_screen_space = make_coordinate_map(truss, screen_bounds, settings.screen_padding);
+    
+    for (id, point) in truss.points.iter() {
+        let (x, y) = {
+            let p = to_screen_space(point.pos());
+            (p.0 + 2., p.1 - 2.)
+        };
+        let name = match truss.names.get(id) {
+            Some(i) => i.as_str(),
+            None => "(unnamed)",
+        };
+        
+        mq::draw_text(name, x, y, 28f32, mq::WHITE);
+    }
+}
+
 /// Begin the control loop for drawing the truss to the screen & handling inputs.
 async fn init_display_truss(truss: Truss2D) {
     loop {
-        draw_truss(&truss);
+        draw_truss(&truss, Default::default());
+        draw_labels(&truss, Default::default());
 
         if mq::is_key_pressed(mq::KeyCode::Escape) {
             break;
@@ -102,7 +151,7 @@ async fn init_display_truss(truss: Truss2D) {
 
 /// Returns the three vertices of a unit triangle, scaled by scale and rotated to point in the
 /// same direction as dir. The vertices are wound clockwise.
-/// 
+///
 /// NOTE: this function already accounts for the conversion to screen space, it will handle flipping
 /// the y-component automatically so that the drawn triangle appears as expected.
 fn triangle_with_rotation(dir: Direction2D, scale: f32) -> [mq::Vec2; 3] {
@@ -119,6 +168,15 @@ fn triangle_with_rotation(dir: Direction2D, scale: f32) -> [mq::Vec2; 3] {
         rotation.rotate(mq::vec2(1.0, -1.0).normalize()) * scale,
         rotation.rotate(mq::vec2(-1.0, -1.0).normalize()) * scale,
     ]
+}
+
+// TODO: add this to the truss drawing function so that it can be drawn in a specified screen
+//  chunk. Potentially even replace padding too
+struct ScreenArea {
+    x_min: f32,
+    x_max: f32,
+    y_min: f32,
+    y_max: f32,
 }
 
 /// Returns a closure that will map coordinates from the truss onto the available screen space, with
@@ -172,6 +230,10 @@ fn make_coordinate_map(
     }
 }
 
+fn create_position_map(truss: &Truss2D, ) -> impl Fn((f64, f64)) -> (f32, f32) {
+    todo!() as fn((f64, f64)) -> (f32, f32)
+}
+
 /// Returns a truss filled with testing values for drawing. This truss is physically impossible and
 /// not in equilibrium. It can not be solved
 pub(crate) fn get_sample_truss() -> Truss2D {
@@ -201,23 +263,29 @@ pub(crate) fn get_sample_truss() -> Truss2D {
         ("D", 25, 225),
         ("E", 25, 75),
     ]
-        .into_iter()
-        .map(|(a, b, c)|{
-            AppliedLoad {
-                at: SolverID::new(a),
-                id: SolverID::new("_"),
-                dir: Direction2D::from_degrees(c as f64),
-                mag: VectorComponent::KnownExactly(b as f64),
-            }
-        })
-        .map(|l| (l.at, l))
-        .collect();
+    .into_iter()
+    .map(|(a, b, c)| AppliedLoad {
+        at: SolverID::new(a),
+        id: SolverID::new("_"),
+        dir: Direction2D::from_degrees(c as f64),
+        mag: VectorComponent::KnownExactly(b as f64),
+    })
+    .map(|l| (l.at, l))
+    .collect();
     let mut supports = HashMap::new();
-    supports.insert(SolverID::new("F"), Support::Pin {at: SolverID::new("F")});
-    supports.insert(SolverID::new("C"), Support::Roller {
-        at: SolverID::new("C"),
-        dir: Direction2D::from_degrees(70.0),
-    });
+    supports.insert(
+        SolverID::new("F"),
+        Support::Pin {
+            at: SolverID::new("F"),
+        },
+    );
+    supports.insert(
+        SolverID::new("C"),
+        Support::Roller {
+            at: SolverID::new("C"),
+            dir: Direction2D::from_degrees(70.0),
+        },
+    );
 
     Truss2D {
         points,
